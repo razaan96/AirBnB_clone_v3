faa359c2070b1c0fa8dedb5436b84a4f21284910
razan <razaan.alaa@hotmail.com>
2024-05-27 18:28:58 +0000

    v2 to v3

---
+++ web_flask/0-hello_route.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""a script that starts a flask web application"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ a function that prints hello `hbnb at the root"""
+    return "Hello HBNB!"
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
